bc32e9c5e70e30ec23cf278b3f5a4150311671bc
Omosuyi Olawole <omooladev@gmail.com>
2024-03-02 07:18:09 +0100

    added a code for outputting the form values

--- Form pages/newsletter/script.js
+++ Form pages/newsletter/script.js
@@ -1,10 +1,9 @@
-let fullname=document.querySelector("#fullname")
-const emailaddress=document.querySelector("#emailaddress")
-const form=document.querySelector(".form")
-form.addEventListener("submit",CLICKED)
+let firstname = document.querySelector("#firstname");
+const emailaddress = document.querySelector("#emailaddress");
+const form = document.querySelector(".form");
+form.addEventListener("submit", CLICKED);
 
-
-function CLICKED(e){
-    e.preventDefault()
-    _fullname
+function CLICKED(e) {
+  e.preventDefault();
+  console.log(firstname.value, emailaddress.value);
 }
