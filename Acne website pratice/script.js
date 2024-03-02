d4cc1ce3f31d33fda311611278cb9f39d7e36f75
Omosuyi Olawole <omooladev@gmail.com>
2024-03-02 06:27:44 +0100

    updated the Acne website files with my own code as the previous one was the one i did while watching the tutorial

---
+++ Acne website pratice/script.js
@@ -0,0 +1,13 @@
+// const Home=document.querySelector("#home")
+// const Body=document.querySelector(".header")
+// Home.addEventListener("click",HOME)
+
+// function HOME(e){
+//     Home.style.cursor="progress";
+//     Home.location.href="index.html"
+    
+//     // Home.style.cursor="pointer";
+//     // console.log("true")
+    
+//     e.preventDefault();
+// }
